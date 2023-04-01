a680b66e563b8981edaf570f76f5b1550535c89e
Maicon Moreira <maiconmoreirapira@gmail.com>
2023-04-01 03:32:04 -0300

    feat: add initial telegram integration

---
+++ src/telegramBot.ts
@@ -0,0 +1,20 @@
+import TelegramBot from "node-telegram-bot-api";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const TELEGRAM_BOT_TOKEN = process.env.TELEGRAM_BOT_TOKEN || "";
+if (!TELEGRAM_BOT_TOKEN) {
+  throw new Error("TELEGRAM_BOT_TOKEN is not defined");
+}
+
+const bot = new TelegramBot(TELEGRAM_BOT_TOKEN, { polling: true });
+
+bot.on("message", (msg) => {
+  const chatId = msg.chat.id;
+
+  // Echo the user's message
+  bot.sendMessage(chatId, "You said: " + msg.text);
+});
+
+export default bot;
